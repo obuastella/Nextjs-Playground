e539a074148e07dacaf8f38a1bcd9a7994138557
Obua Stella <stellaobua123@gmail.com>
2024-03-12 11:59:05 +0100

    Learnt alias

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+import Header from "@/components/Header";
 export default function page() {
   return (
     <>
+      <Header />
       <h1 className="text-center text-4xl m-8">Hello world</h1>
+      <Link href="/meals">click me!!</Link>
     </>
   );
 }
