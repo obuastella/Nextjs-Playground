01cce222ac0200cf1738365b799233c606e7acde
Obua Stella <stellaobua123@gmail.com>
2024-03-12 12:18:49 +0100

    Learnt Dynamic routes

---
+++ app/src/app/meals/[mealSlug]/page.tsx
@@ -0,0 +1,11 @@
+// when the user enters something
+// localhost:3000/meals/something
+
+// but that something that is not
+// localhost:3000/meals/share ---nextjs is smart
+
+// This page is called a dynamic segment
+// what ever url you put after /meals will display this page!
+export default function MealDefaultsPage() {
+  return <h1>Meal Details</h1>;
+}
